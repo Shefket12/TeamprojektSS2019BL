664f2a99290b096baf89cac2635c16d15780693b
DanielDauner <50077664+DanielDauner@users.noreply.github.com>
2019-06-28 16:05:44 +0200

    Update Crawlerpy.py
    The function get(self, FirstSeason, LastSeason): now directly writes the games between FirstSeason and LastSeason in the csv.-file together with the season and the gameday

--- Crawler/Crawlerpy.py
+++ Crawler/Crawlerpy.py
@@ -9,9 +9,8 @@ except:
 class DataCrawler:
 
     def __init__(self, csvPath):
-        self.Matchdays = []
         self.csvPath = csvPath
-	
+        
     #method, to check the internet-connection, returning True and False as resulst
     #only the HEAD and no HTML will be fetched
     def internet_on(self):
@@ -23,85 +22,63 @@ class DataCrawler:
         except:
             conn.close()
             return False
-        
-    def clear_Matchdays(self):
-        self.Matchdays = []
-        
-    def add_Season(self, SeasonYear, FirstMatchday, LastMatchday):
+     
+    def get(self, FirstSeason, LastSeason):
+        csv = open(self.csvPath, "w") 
         if(self.internet_on() == False):
             raise Exception('You should check your internet connection, before you proceed')
-        else:
-            for i in range(FirstMatchday, LastMatchday):
-                self.Matchdays.append(requests.get(f'http://www.openligadb.de/api/getmatchdata/bl1/{SeasonYear}//{i}'))
         
-    def write_CSVFile(self):
-        csv = open(self.csvPath, "w") 
-  
-    #the following are iterable variables, i.e.: Hometeam[1] != Hometeam[2] -> thats also the way, you call a variable
-        counter = 0
-        Game = {}
-        Date = {}
-    	HTID = {}
-    	ATID = {}
-        GoalsHome = {}
-	GoalsHome1 = 0
-	GoalsHome2 = 0
-        GoalsAway = {}
-	GoalsAway1 = 0
-	GoalsAway2 = 0
-
-        for days in self.Matchdays:
-        #todos is a local variable, that safes the current matchday
-            todos = json.loads(days.text)
-	
-        #to write the current matchday and in the next cell the given names for the rows into the csv-file
-	#this part is only for clarity reasons, if one wants to take a look at the data
-        #Mday = f"Matchday{i}" + "," + "," + "," + "," + "\n"
-        #csv.write(Mday)
-        #columnTitleRow = "DateOfGame,HomeTeam,AwayTeam,GoalsScoredHome,GoalsScoredAway\n"
-        #csv.write(columnTitleRow)
-	
-        # The todos has different tuples Like: [Team1:{'Name':"...",'TeamID':"...", 'ShortName':"..."}]
-        # To get different values we save the lists in different vars and then go through the lists in the steps after that with
-        # 'countable'-variables
-            for game in todos:
-            #Safes the single games in an agile variable
-                Game[counter] = game
-                for team in Game[counter]:
-                #Safe the date
-                    Date[counter] = Game[counter]['MatchDateTime']
+        else:
+            for i in range(FirstSeason, (LastSeason+1)):
+                for j in range(0, 35):
+                    counter = 0
+                    Game = {}
+                    Date = {}
+                    HTID = {}
+                    ATID = {}
+                    GoalsHome = {}
+                    GoalsHome1 = 0
+                    GoalsHome2 = 0
+                    GoalsAway = {}
+                    GoalsAway1 = 0
+                    GoalsAway2 = 0
+                
+                    todos = json.loads(requests.get(f'http://www.openligadb.de/api/getmatchdata/bl1/{i}//{j}').text)
+        
+                    for game in (todos):
+                        Game[counter] = game
+                        for team in Game[counter]:
+                            Date[counter] = Game[counter]['MatchDateTime']
     
-                #Safe Teamnames and TeamIDs, by going through the list
-                    Team1 = Game[counter]['Team1']
-                    HTID[counter] = Team1['TeamId']
-                
-                    Team2 = Game[counter]['Team2']
-                    ATID[counter] = Team2['TeamId']
+                                      
+                            Team1 = Game[counter]['Team1']
+                            HTID[counter] = Team1['TeamId']
 
-                #Safe scored goals by team, by going through the list
-                #check, if the finalscore is in Matchresults[0] or Matchresults[1]
-                    Matchresults = Game[counter]['MatchResults']
+                            Team2 = Game[counter]['Team2']
+                            ATID[counter] = Team2['TeamId']
 
-                    Result = Matchresults[0]
-                    GoalsHome1 = Result['PointsTeam1']
-                    GoalsAway1 = Result['PointsTeam2']
+                                    #Safe scored goals by team, by going through the list
+                                    #check, if the finalscore is in Matchresults[0] or Matchresults[1]
+                            Matchresults = Game[counter]['MatchResults']
 
-                    Result = Matchresults[1]
-                    GoalsHome2 = Result['PointsTeam1']
-                    GoalsAway2 = Result['PointsTeam2']
-                    if(GoalsHome1+GoalsAway1 >= GoalsHome2+GoalsAway2):
-                        Result = Matchresults[0]
-                        GoalsHome[counter] = Result['PointsTeam1']
-                        GoalsAway[counter] = Result['PointsTeam2']
-                    elif(GoalsHome2+GoalsAway2 > GoalsHome1+GoalsAway1):
-                        Result = Matchresults[1]
-                        GoalsHome[counter] = Result['PointsTeam1']
-                        GoalsAway[counter] = Result['PointsTeam2']
+                            Result = Matchresults[0]
+                            GoalsHome1 = Result['PointsTeam1']
+                            GoalsAway1 = Result['PointsTeam2']
 
-            #to understand what is done here, look at the comments above @ the csv file
-            #the empty string is put in, so that there can be differenciated between Home- and Awayteam    
-                rowGame = Date[counter] + "," + str(HTID[counter]) +  "," +  str(ATID[counter]) +  "," +str(GoalsHome[counter]) + "," + str(GoalsAway[counter]) +"\n"
-                csv.write(rowGame)
-
-                counter += 1
-                
+                            Result = Matchresults[1]
+                            GoalsHome2 = Result['PointsTeam1']
+                            GoalsAway2 = Result['PointsTeam2']
+                                        
+                            if(GoalsHome1+GoalsAway1 >= GoalsHome2+GoalsAway2):
+                                Result = Matchresults[0]
+                                GoalsHome[counter] = Result['PointsTeam1']
+                                GoalsAway[counter] = Result['PointsTeam2']
+                            elif(GoalsHome2+GoalsAway2 > GoalsHome1+GoalsAway1):
+                                Result = Matchresults[1]
+                                GoalsHome[counter] = Result['PointsTeam1']
+                                GoalsAway[counter] = Result['PointsTeam2']
+                            
+                        rowGame = Date[counter]+","+str(i)+","+str(j)+"," + str(HTID[counter]) +  "," +  str(ATID[counter]) +  "," +str(GoalsHome[counter]) + "," + str(GoalsAway[counter]) +"\n"
+                            
+                        csv.write(rowGame)
+                        counter += 1

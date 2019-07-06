0643f9386e3b795080fff7827db7f88988c075e5
DanielDauner <50077664+DanielDauner@users.noreply.github.com>
2019-07-06 17:13:23 +0200

    Update Crawlerpy.py

--- Crawler/Crawlerpy.py
+++ Crawler/Crawlerpy.py
@@ -11,6 +11,7 @@ class DataCrawler:
     def __init__(self, csvPath):
         self.csvPath = csvPath
         
+        
     #method, to check the internet-connection, returning True and False as resulst
     #only the HEAD and no HTML will be fetched
     def internet_on(self):
@@ -22,64 +23,76 @@ class DataCrawler:
         except:
             conn.close()
             return False
-     
-    def get(self, FirstSeason, LastSeason):
+    
+    def clear(self):
         csv = open(self.csvPath, "w")
-        csv.write("Date" + "," + "," + "," + "HomeTeam" + "," + "AwayTeam" + "," + "GoalsHome" + "," + "GoalsAway" + "\n")
+        
+        
+                        
+    def getSeasons(self, FirstSeason, LastSeason):
+        csv = open(self.csvPath, "w")
+        csv.write("Date" + "," + "Season" + "," + "GameDay" +"," + "HomeTeam" + "," + "AwayTeam" + "," + "GoalsHome" + "," + "GoalsAway" + "\n")
         if(self.internet_on() == False):
             raise Exception('You should check your internet connection, before you proceed')
         
         else:
             for i in range(FirstSeason, (LastSeason+1)):
-                for j in range(0, 35):
-                    counter = 0
-                    Game = {}
-                    Date = {}
-                    HTID = {}
-                    ATID = {}
-                    GoalsHome = {}
-                    GoalsHome1 = 0
-                    GoalsHome2 = 0
-                    GoalsAway = {}
-                    GoalsAway1 = 0
-                    GoalsAway2 = 0
+                counter = 0
+                Game = {}
                 
-                    todos = json.loads(requests.get(f'http://www.openligadb.de/api/getmatchdata/bl1/{i}//{j}').text)
+                Date = {}
+                Season = {}
+                GameDay = {}
+                HomeTeam = {}
+                AwayTeam = {}
+                
+                GoalsHome = {}
+                GoalsHome1 = 0
+                GoalsHome2 = 0
+                GoalsAway = {}
+                GoalsAway1 = 0
+                GoalsAway2 = 0
+                
+                todos = json.loads(requests.get(f'http://www.openligadb.de/api/getmatchdata/bl1/{i}').text)
         
-                    for game in (todos):
-                        Game[counter] = game
-                        for team in Game[counter]:
-                            Date[counter] = Game[counter]['MatchDateTime']
+                for game in (todos):
+                    Game[counter] = game
+                    for team in Game[counter]:
+                        
+                        Date[counter] = Game[counter]['MatchDateTime']
+                        Group = Game[counter]['Group']
+                        GameDay[counter] = Group['GroupOrderID']
+                        
     
                                       
-                            Team1 = Game[counter]['Team1']
-                            HTID[counter] = Team1['TeamId']
+                        Team1 = Game[counter]['Team1']
+                        HomeTeam[counter] = Team1['TeamName']
 
-                            Team2 = Game[counter]['Team2']
-                            ATID[counter] = Team2['TeamId']
+                        Team2 = Game[counter]['Team2']
+                        AwayTeam[counter] = Team2['TeamName']
 
                                     #Safe scored goals by team, by going through the list
                                     #check, if the finalscore is in Matchresults[0] or Matchresults[1]
-                            Matchresults = Game[counter]['MatchResults']
+                        Matchresults = Game[counter]['MatchResults']
 
-                            Result = Matchresults[0]
-                            GoalsHome1 = Result['PointsTeam1']
-                            GoalsAway1 = Result['PointsTeam2']
+                        Result = Matchresults[0]
+                        GoalsHome1 = Result['PointsTeam1']
+                        GoalsAway1 = Result['PointsTeam2']
 
-                            Result = Matchresults[1]
-                            GoalsHome2 = Result['PointsTeam1']
-                            GoalsAway2 = Result['PointsTeam2']
+                        Result = Matchresults[1]
+                        GoalsHome2 = Result['PointsTeam1']
+                        GoalsAway2 = Result['PointsTeam2']
                                         
-                            if(GoalsHome1+GoalsAway1 >= GoalsHome2+GoalsAway2):
-                                Result = Matchresults[0]
-                                GoalsHome[counter] = Result['PointsTeam1']
-                                GoalsAway[counter] = Result['PointsTeam2']
-                            elif(GoalsHome2+GoalsAway2 > GoalsHome1+GoalsAway1):
-                                Result = Matchresults[1]
-                                GoalsHome[counter] = Result['PointsTeam1']
-                                GoalsAway[counter] = Result['PointsTeam2']
+                        if(GoalsHome1+GoalsAway1 >= GoalsHome2+GoalsAway2):
+                            Result = Matchresults[0]
+                            GoalsHome[counter] = Result['PointsTeam1']
+                            GoalsAway[counter] = Result['PointsTeam2']
+                        elif(GoalsHome2+GoalsAway2 > GoalsHome1+GoalsAway1):
+                            Result = Matchresults[1]
+                            GoalsHome[counter] = Result['PointsTeam1']
+                            GoalsAway[counter] = Result['PointsTeam2']
                             
-                        rowGame = Date[counter]+","+str(i)+","+str(j)+"," + str(HTID[counter]) +  "," +  str(ATID[counter]) +  "," +str(GoalsHome[counter]) + "," + str(GoalsAway[counter]) +"\n"
+                    rowGame = Date[counter]+","+str(i)+","+str(GameDay[counter])+","+ HomeTeam[counter] +  "," +  AwayTeam[counter] +  "," +str(GoalsHome[counter]) + "," + str(GoalsAway[counter]) +"\n"
                             
-                        csv.write(rowGame)
-                        counter += 1
+                    csv.write(rowGame)
+                    counter += 1

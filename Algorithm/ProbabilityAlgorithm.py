fa22b2a05908d73ec6fa482e999837645a0102d5
Shefket12 <50022674+Shefket12@users.noreply.github.com>
2019-07-11 16:43:04 +0200

    Add files via upload

--- Algorithm/ProbabilityAlgorithm.py
+++ Algorithm/ProbabilityAlgorithm.py
@@ -6,6 +6,7 @@
 import sys
 from Algorithm.parse_csv_data import parse
 from Algorithm.base_probability import*
+from Algorithm.poisson_algo import *
 import pandas as pd
 import numpy as np
 from scipy.stats import poisson
@@ -42,55 +43,6 @@ class ProbabilityAlgorithm:
             return True
         else:
             return False
-           
-    #replace the IDs of each team with their actual name, for pandas
-    def replaceIDs(self,dataframe):
-        teamIDs = "Data/TeamIDs.csv"
-        with open(teamIDs) as csv_file:
-            csv_reader = csv.reader(csv_file, delimiter=',')            
-            for row in csv_reader:
-                dataframe['HomeTeam'].replace(int(row[0]), row[1],inplace=True)
-                dataframe['AwayTeam'].replace(int(row[0]), row[1],inplace=True)
-    
-    # compute tetas for both teams for poisson regression
-    def computeTeta(self, filename):
-        data = pd.read_csv(filename)
-        #self.replaceIDs(data)
-        goal_model_data =  pd.concat([data[['HomeTeam', 'AwayTeam', 'GoalsHome']].assign(Home=1).rename(columns={'HomeTeam': 'Team', 'AwayTeam': 'Opponent', 'GoalsHome': 'Goals'}),data[['AwayTeam', 'HomeTeam', 'GoalsAway']].assign(Home=0).rename(columns={'AwayTeam': 'Team', 'HomeTeam': 'Opponent', 'GoalsAway': 'Goals'})])
-        poisson_model = smf.glm(formula='Goals ~ Home + Team + Opponent', data=goal_model_data, family=sm.families.Poisson()).fit()
-        return poisson_model
-
-    # computes and Array with the probabilities for different scores between two given teams, i.e. Bayern 3-1 BVB 5,39%
-    def simulateMatch(self, homeTeam, awayTeam, filename):
-        max_goals=10
-        poisson_model = self.computeTeta(filename)
-        home_goals_avg = poisson_model.predict(pd.DataFrame(data={'Team': homeTeam, 
-                                                                'Opponent': awayTeam,'Home':1},
-                                                          index=[1])).values[0]
-        away_goals_avg = poisson_model.predict(pd.DataFrame(data={'Team': awayTeam, 
-                                                                'Opponent': homeTeam,'Home':0},
-                                                          index=[1])).values[0]
-        team_pred = [[poisson.pmf(i, team_avg) for i in range(0, max_goals+1)] for team_avg in [home_goals_avg, away_goals_avg]]
-        return(np.outer(np.array(team_pred[0]), np.array(team_pred[1])))
-
-    # these methods are the same, just with different returns. Give out the probabilit for a team to win or a draw between the teams
-
-    def computeWinProbHome(self, HomeTeam, AwayTeam, filename):
-        print(filename)
-        game = self.simulateMatch(HomeTeam, AwayTeam, filename)
-        winHome = np.sum(np.tril(game, -1))
-        return winHome
-
-    def computeWinProbAway(self, HomeTeam, AwayTeam, filename):
-        game = self.simulateMatch(HomeTeam, AwayTeam, filename)
-        winAway = np.sum(np.triu(game, 1))
-        return winAway
-
-    def computeDraw(self, HomeTeam, AwayTeam, filename):
-        game = self.simulateMatch(HomeTeam, AwayTeam, filename)
-        draw = np.sum(np.diag(game, -1))
-        return draw
-    
         
     def getResult(self,HomeTeam, GuestTeam, filename):
         
@@ -104,7 +56,7 @@ class ProbabilityAlgorithm:
             # Poisson regression, filename
             elif(self.algorithmIndex == 1):
                 
-                probabilities = [self.computeWinProbHome(HomeTeam, GuestTeam, filename), self.computeWinProbAway(HomeTeam, GuestTeam, filename), self.computeDraw(HomeTeam, GuestTeam, filename)]
+                probabilities = [computeWinProbHome(self.matches, HomeTeam, GuestTeam, filename), computeWinProbAway(self.matches, HomeTeam, GuestTeam, filename), computeDraw(self.matches, HomeTeam, GuestTeam, filename)]
                 
             else:
                 sys.stderr.write("No algorithm given for the calculation")

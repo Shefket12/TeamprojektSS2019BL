ced449cd8d8ab26add23f050cb14d52892f2fb33
Shefket12 <50022674+Shefket12@users.noreply.github.com>
2019-07-12 10:48:02 +0200

    Add files via upload

--- Data/writeTeamIDs.py
+++ Data/writeTeamIDs.py
@@ -1,11 +1,11 @@
 import csv
 
 def writeTeamIDs():
-    Teams = {79: '1.FC Nürnberg', 81: 'Mainz 05', 6: 'Levverkusen', 7: 'BVB', 87: 'BMG', 
-        91: 'Frankfurt', 95: 'Augsburg', 40: 'FC Bayern', 9: 'FC Schalke', 185: 'Düsseldorf', 
-        55: 'Hannover 96', 54: 'Hertha BSC', 1635: 'RB Leipzig', 112: 'Freiburg', 123: 'TSG', 
-        16: 'Stuttgart', 131: 'Wolfsburg', 134: 'Bremen'}
-    download_dir = "TeamIDs.csv"
+    Teams = {79: '1.FC Nürnberg', 81: '1. FSV Mainz 05', 6: 'Bayer Leverkusen', 7: 'Borussia Dortmund', 87: 'Borussia Mönchengladbach', 
+        91: 'Eintracht Frankfurt', 95: 'FC Augsburg', 40: 'FC Bayern', 9: 'FC Schalke 04', 185: 'Fortuna Düsseldorf', 
+        55: 'Hannover 96', 54: 'Hertha BSC', 1635: 'RB Leipzig', 112: 'SC Freiburg', 123: 'TSG 1899 Hoffenheim', 
+        16: 'Stuttgart', 131: 'VfL Wolfsburg', 134: 'Werder Bremen', 31: 'SC Paderborn 07', 80: '1. FC Union Berlin', 65: '1. FC Köln'}
+    download_dir = "C:\\Users\\Shefket\\Documents\\Uni\\SS19\\Teamprojekt\\Data\\TeamIDs.csv"
     csv = open(download_dir, 'w')
     for i in Teams:
         wholeTeam = str(i) + ',' + Teams[i] + '\n'
